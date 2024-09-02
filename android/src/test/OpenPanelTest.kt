0eba5b7deefccd0826a8362f3656f77bcbb7e368
Carl-Gerhard Lindesvärd <lindesvard@gmail.com>
2024-09-03 00:14:44 +0200

    wip

--- android/src/test/OpenPanelTest.kt
+++ android/src/test/OpenPanelTest.kt
@@ -7,18 +7,16 @@ class OpenPanelTest {
 
     @Test
     fun testPerformOperation() {
+        println("test")
         val openPanel = OpenPanel(OpenPanel.Options(
-            clientId = "0f6ceb93-ba13-4106-858e-c44d48229899",
-            clientSecret = "sec_9679b09055dcb5b91597",
+            clientId = "",
+            clientSecret = "",
             waitForProfile = false,
             disabled = false,
             automaticTracking = false
         ))
         openPanel.track("test_event")
-        openPanel.initialize()
-        println("test")
         Thread.sleep(3000)
-        println("test")
         assertEquals("Result for test", "Hej")
     }
 }
